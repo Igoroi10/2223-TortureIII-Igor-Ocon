61079984c2b6ef4daa5f530035829a7a337e5fd5
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2023-03-29 10:09:38 +0200

    [UPDATE] getDataBaseInfo working and getting 91 ingredients

--- src/events.js
+++ src/events.js
@@ -1,11 +1,24 @@
-import globals from "./globals";
+import globals from "./globals.js";
 import {createPotion,} from "./logic.js"
-import {getDataBaseInfo} from ".initialize.js"
+import {getDataBaseInfo} from "./initialize.js"
 
 function btnStartDown(){
-    globals.potionsData = getDataBaseInfo();
+    getDataBaseInfo();
 }
 
 function btnCreateDown(){
     createPotion()
+}
+
+function displaySecondPage(){
+
+    document.getElementById('potionIngredients').style.display  = "block";
+    document.getElementById('startDiv').style.display           = "none";
+}
+
+
+export{
+    btnCreateDown,
+    btnStartDown,
+    displaySecondPage,
 }
--- src/initialize.js
+++ src/initialize.js
@@ -23,6 +23,41 @@ function initEvents(){
 
 function getDataBaseInfo(){
 
+    //Ruta o absoluta o relativa al fichero que hace la petición (html)
+    const url = "https://raw.githubusercontent.com/zsiciarz/skyrim-alchemy-toolbox/master/data/ingredients.json";
+    const request = new XMLHttpRequest();
+
+    request.onreadystatechange = function (){
+
+        if(this.readyState == 4){
+
+            if(this.status == 200){
+
+                if(this.responseText != null){
+
+                    const resultJSON = JSON.parse(this.responseText);
+                    
+                    globals.potionsData = resultJSON;
+
+                    console.log(globals.potionsData);
+
+                    displaySecondPage();
+
+                }
+                
+            else
+                alert("Communication error: No data received");
+            }
+
+            else
+                alert("Communication error: " + this.statusText);
+        }
+    }
+
+    request.open('GET', url, true);
+    request.responseType = "text";
+    request.send();
+    
 }
 
 export {
